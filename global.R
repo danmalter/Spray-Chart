c8c4a46e0bfa5346c93839690e0c2c2365ae1d96
Danny <danmalter@gmail.com>
2015-03-22 12:16:10 -0500

    update

--- global.R
+++ global.R
@@ -7,20 +7,20 @@ library(shinydashboard)
 library(Lahman)
 
 
-   files <- c("inning/inning_hit.xml", "miniscoreboard.xml", "players.xml")
-   my_db <- src_sqlite("MLB2014.sqlite3", create = TRUE)
-   scrape(start = "2014-03-30", end = "2014-09-30", connect = my_db$con, suffix = files)
+files <- c("inning/inning_hit.xml", "miniscoreboard.xml", "players.xml")
+my_db <- src_sqlite("MLB2014.sqlite3", create = TRUE)
+scrape(start = "2014-03-30", end = "2014-09-30", connect = my_db$con, suffix = files)
    
-   locations <- select(tbl(my_db, "hip"), des, x, y, batter, pitcher, type, team, inning)
-   locations <- as.data.frame(locations)
-   batters <- select(tbl(my_db, "player"), first, last, id, bats, team_abbrev)
-   batters <- as.data.frame(batters, n=-1)
-   batters$full.name <- paste(batters$first, batters$last, sep = " ")
-   names(batters)[names(batters) == 'id'] <- 'batter'
+locations <- select(tbl(my_db, "hip"), des, x, y, batter, pitcher, type, team, inning)
+locations <- as.data.frame(locations)
+batters <- select(tbl(my_db, "player"), first, last, id, bats, team_abbrev)
+batters <- as.data.frame(batters, n=-1)
+batters$full.name <- paste(batters$first, batters$last, sep = " ")
+names(batters)[names(batters) == 'id'] <- 'batter'
    
-   spraychart <- merge(locations, batters, by="batter") 
-   names(spraychart)[names(spraychart) == 'des'] <- 'Description'
-
+# merge was not working properly, I wrote both files (batters and locations) to a csv and did a vlookup in Excel. 
+spraychart <- merge(locations, batters, by="batter") 
+names(spraychart)[names(spraychart) == 'des'] <- 'Description'
 
 # For ease of loading into a Shiny app, I uploaded spraychart into Dropbox, did a bit of cleaning and reloaded the csv file from there.
 spraychart <- paste("https://www.dropbox.com/s/nlunrtjgniw3bmg/spraycharts.csv?dl=0")
